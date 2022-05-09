40370c89596a9681213535e36cf7f689b063b000
VinceV0 <30036567@students.toiohomai.ac.nz>
2022-05-10 10:10:38 +1200

    Added view balance option

--- ATM Console App/Program.cs
+++ ATM Console App/Program.cs
@@ -11,9 +11,11 @@ namespace ATM_Console_App
         // Our allowed user input
         public static string[] menuOptions = {"View Balance", "Withdraw Funds", "Deposit Funds", "Exit" };
         //User balance of funds
-        public static decimal balance = 0;
+        public static decimal balance = 100.00M;
         //PIN constant global variable
         public const string PIN = "1234";
+        //Global variable holds user input
+        public static string userInput;
 
         static void CheckPin()
         {
@@ -47,7 +49,36 @@ namespace ATM_Console_App
                 Console.WriteLine("[" + (i+1).ToString() + "] "+ menuOptions[i]);
             }
             Console.WriteLine("*******************************\n");
+            ProcessReq();
         }
+
+        static void ProcessReq()
+        {
+            userInput = Console.ReadLine();
+            if (userInput == "1")
+            {
+                Console.WriteLine(menuOptions[0] + " selected");
+                Console.WriteLine("Your balance is: " + balance.ToString("c"));
+            }
+            else if (userInput == "2")
+            {
+                Console.WriteLine(menuOptions[1] + " selected");
+            }
+            else if (userInput == "3")
+            {
+                Console.WriteLine(menuOptions[2] + " selected");
+            }
+            else if (userInput == "4")
+            {
+                Console.WriteLine(menuOptions[3] + " selected");
+            }
+            else
+            {
+                Console.WriteLine("Not a valid input try again");
+                ProcessReq();
+            }
+        }
+
         static void Main(string[] args)
         {
             CheckPin();
